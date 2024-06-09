fcb7952db3df84a0f52a1e396812b946e365b8ca
Rizanriz <rizanr7777@gmail.com>
2024-06-09 17:19:07 +0530

    deployment

--- frondend/vite.config.js
+++ frondend/vite.config.js
@@ -4,17 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,  // Ensure changeOrigin is set to true for correct proxying
-        rewrite: (path) => path.replace(/^\/api/, '')
+  server:{
+    port:3000,
+    proxy:{
+      "/api":{
+        target:"http://localhost:5000"
       }
     }
-  },
-  build: {
-    outDir: 'dist'  // Ensure the output directory is correctly set
   }
 })
+  
